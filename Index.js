2f37dc526cb1ab493fc88581ebdc8d64d5532edc
Otagada Isaac <otagadaisaac@gmail.com>
2022-10-03 15:41:47 +0100

    More Updates
    - Added navigation icon animation
    - Edited the responsive navigation icon on 2 devices

--- Index.js
+++ Index.js
@@ -2,4 +2,5 @@ let navigation = document.querySelector(".nav")
 let ham = document.querySelector(".ham")
 ham.addEventListener('click', ()=>{
     navigation.classList.toggle("active")
+    ham.classList.toggle("active")
 })
